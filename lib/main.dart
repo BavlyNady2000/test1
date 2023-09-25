bb31797b5889f5f63b159e4c264858434ec55fb6
BAVLY <bavlynady4@gmail.com>
2023-09-25 12:03:58 +0200

    add homescreen empty

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
+
 import 'homescreen.dart';
 
 void main() {
   runApp(myapp());
-}
+   }
 
 class myapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: homescreen(), debugShowCheckedModeBanner: false); //MaterialApp
+    return MaterialApp(home: homescreen(), debugShowCheckedModeBanner: false);
+    //MaterialApp
   }
 }
